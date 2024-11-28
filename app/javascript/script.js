3e284c35c44f98c4c400651ce29de8b6796a84bb
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-11-28 11:21:38 +0000

    [Add]Swiper実装

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -8,3 +8,20 @@ $(document).ready(function(){
         $(this).css('color','red');
     });
 })
+
+// Swiperのオプションを定義　（const = opt）
+const opt = {
+    loop: true,
+    pagination:{
+        el: '.swiper-paginaion',
+    },
+    navigation:{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+}
+
+// Swiperの実行部分
+$(document).on('turbolinks:load', function(){
+    let swiper = new Swiper(".swiper",opt);
+});
